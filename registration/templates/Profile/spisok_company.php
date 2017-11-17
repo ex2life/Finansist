820e1720a38f1b3bdf2ddc81d8f30c4d9d35f6c4
ex2life <abramizsaransk@gmail.com>
2017-11-17 21:01:39 +0300

    list of company

--- registration/templates/Profile/spisok_company.php
+++ registration/templates/Profile/spisok_company.php
@@ -31,6 +31,7 @@
 			<th>ОПФ</th>
 			<th>ИНН</th>
 			<th>СНО</th>
+			<th>Команды</th>
 		</tr>
 		<?php foreach ($company_list as $i => $company): ?>
 		<tr class="<?= ($i+1)%2 == 0 ? 'even' : 'odd' ?>">
@@ -38,6 +39,10 @@
 			<td title="<?= $company['opf_full_name'] ?>"><?= $company['opf_brief_name'] ?></td>
 			<td><?= $company['inn'] ?></td>
 			<td title="<?= $company['sno_full_name'] ?>"><?= $company['sno_brief_name'] ?></td>
+		    <td>
+                <button type="submit" name="Edit" value="<?= $company['company_id'] ?>" /> <image width="20px" height="20px" src="../img/edit.png"> </button>
+                <button type="submit" name="Del" value="<?= $company['company_id'] ?>" onclick="if(confirm(\'Точно желаете удалить?\'))submit();else return false;" /> <image width="20px" height="20px" src="../img/del.png"> </button>
+            </td>
 		</tr>
 		<?php endforeach; ?>
 	</table>
